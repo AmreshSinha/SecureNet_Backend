54ee68cc651aea752928689c3a461b566f0b6d9a
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-12-19 05:15:24 +0000

    Add type key in json res

--- ipdata_utils.py
+++ ipdata_utils.py
@@ -8,4 +8,6 @@ load_dotenv()
 def ip_report(ip: str):
     response = requests.get(
         f"https://api.ipdata.co/{ip}/threat?api-key={os.environ['IPDATA_API_KEY']}")
-    return response.json()
+    response = response.json()
+    response['type'] = 'ip'
+    return response
