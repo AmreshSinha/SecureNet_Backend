a3d243ea52e73084dbeca80ee42df77ab722c5f5
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-12-20 10:03:25 +0000

    Add spamhaus try except

--- spamhaus_utils.py
+++ spamhaus_utils.py
@@ -6,6 +6,7 @@ import tldextract
 
 load_dotenv()
 
+
 def spamhaus_token() -> str:
     if check_spamhaus_token():
         print("SpamHaus Token: Cache Hit!")
@@ -23,18 +24,25 @@ def spamhaus_token() -> str:
             return token
         # return str(token, 'UTF-8')
 
+
 def domain_report(domain: str):
-    token = spamhaus_token()
-    # curl -s https://api.spamhaus.org/api/intel/v2/byobject/domain/google.com -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI4Njg1NDksImV4cCI6MTcwMjk1NDk0OSwiaXNzIjoic2hhcGktYXV0aCIsImF1ZCI6ImludGVsIiwic3ViIjoiMjkwOTE3OTUiLCJ1c3IiOiJhbXJlc2hAZHVjay5jb20iLCJ0aWVyIjp7Imx2bCI6InN0ZCIsImFkcyI6IlhCTCxCQ0wsQ1NTLERPTUFJTiIsImFkdyI6MCwicW1zIjo1MDAwLCJxbWgiOjUwMDAsInJsX3FwaCI6NTAwMCwicmxfcXBtIjo1MDAwLCJybF9xcHMiOjUwMDB9fQ.jJGLNZwpZB_bfDEDHBg8yBw7zk75AKzht4vJcguvtsY'
-    print(f"Domain Report: {domain}")
-    extracted = tldextract.extract(domain)
-    response = requests.get(
-        f"https://api.spamhaus.org/api/intel/v2/byobject/domain/{'{}.{}'.format(extracted.domain, extracted.suffix)}",
-        headers={
-            "Authorization": f"Bearer {token}"
+    try:
+        token = spamhaus_token()
+        # curl -s https://api.spamhaus.org/api/intel/v2/byobject/domain/google.com -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI4Njg1NDksImV4cCI6MTcwMjk1NDk0OSwiaXNzIjoic2hhcGktYXV0aCIsImF1ZCI6ImludGVsIiwic3ViIjoiMjkwOTE3OTUiLCJ1c3IiOiJhbXJlc2hAZHVjay5jb20iLCJ0aWVyIjp7Imx2bCI6InN0ZCIsImFkcyI6IlhCTCxCQ0wsQ1NTLERPTUFJTiIsImFkdyI6MCwicW1zIjo1MDAwLCJxbWgiOjUwMDAsInJsX3FwaCI6NTAwMCwicmxfcXBtIjo1MDAwLCJybF9xcHMiOjUwMDB9fQ.jJGLNZwpZB_bfDEDHBg8yBw7zk75AKzht4vJcguvtsY'
+        print(f"Domain Report: {domain}")
+        extracted = tldextract.extract(domain)
+        response = requests.get(
+            f"https://api.spamhaus.org/api/intel/v2/byobject/domain/{'{}.{}'.format(extracted.domain, extracted.suffix)}",
+            headers={
+                "Authorization": f"Bearer {token}"
+            }
+        )
+        print("Domain Report", response.json())
+        response = response.json()
+    except:
+        response = {
+            "domain": domain,
+            "score": 0,
         }
-    )
-    print("Domain Report", response.json())
-    response = response.json()
     response['type'] = 'domain'
     return response
