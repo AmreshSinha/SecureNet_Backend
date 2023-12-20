edf351e647d229c30b1dc1128eba350ec13ee909
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-12-20 09:38:15 +0000

    FIX AGAIN!

--- ipdata_utils.py
+++ ipdata_utils.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 def ip_report(ip: str):
     response = requests.get(
-        f"https://api.ipdata.co/{ip}/threat?api-key={os.environ['IPDATA_API_KEY']}")
+        f"https://api.ipdata.co/{ip}?api-key={os.environ['IPDATA_API_KEY']}")
     response = response.json()
     return response
